2d6bd10d421dc82e65ccbde63f716e44f9f06f4e
Union One <hax2033@gmail.com>
2016-03-02 16:20:22 +0300

    Introducing to inject spring shit to bean

--- src/main/java/com/itibo/spring/bean/UserBean.java
+++ src/main/java/com/itibo/spring/bean/UserBean.java
@@ -2,9 +2,15 @@ package com.itibo.spring.bean;
 
 import com.itibo.spring.bean.bo.UserBo;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
 /**
  * Created by union on 02.03.2016.
  */
+
+@ManagedBean(name = "userBean")
+@SessionScoped
 public class UserBean {
     UserBo userBo;
 
